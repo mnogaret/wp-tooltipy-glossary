3ab76a6efc01a4c36ac4ab8ea2c7a1f3a8bfc210
Mathieu Nogaret <mathieu.nogaret@gmail.com>
2025-11-17 10:07:15 +0100

    Debug

--- custom-tooltipy-glossary.php
+++ custom-tooltipy-glossary.php
@@ -11,6 +11,8 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
+define( 'WP_DEBUG', true );
+
 add_action( 'init', function() {
     register_taxonomy(
         'tooltipy_category',
@@ -66,7 +68,7 @@ function custom_tooltipy_glossary( $atts ) {
 
     while ( $q->have_posts() ) {
 	$q->the_post();
-	$pos_id = get_the_ID();
+	$pos_id = get_the_id();
         $slug = get_post_field( 'post_name' );
 
 	echo '<dt id="glossary-' . est_attr( $slug ) . '">';
