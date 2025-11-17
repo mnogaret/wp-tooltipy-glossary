ebc5d838dd0814482dd049bf269f87b0e2dab917
Mathieu Nogaret <mathieu.nogaret@gmail.com>
2025-11-17 11:11:36 +0100

    Letters

--- custom-tooltipy-glossary.php
+++ custom-tooltipy-glossary.php
@@ -46,28 +46,10 @@ function custom_tooltipy_glossary( $atts ) {
         $text_all = __('ALL','tooltipy-lang');
     }
 
-    $current_letter_class = '';
-    if ( empty ( $_GET['letter'] ) ) {
-        $current_letter_class = 'bluet_glossary_current_letter';
-    }
-
-    $all_link = get_permalink();
-
-    $ret = "<div class='kttg_glossary_div'>";
-
-//    $ret. = '<div class="kttg_glossary_header"><span class="bluet_glossary_all ' . $current_letter_class . '"><a href=\'' . $all_link . '\'>' . $text_all . '</a></span> - ';
-
-    $args = [
-        'post_type'      => 'my_keywords',
-        'post_status'    => 'publish',
-        'posts_per_page' => -1,
-        'orderby'        => $atts['orderby'],
-        'order'          => $atts['order'],
-    ];
-
+    $tax_query = array();
     if ( $atts['cat'] !== '' ) {
         $terms = array_map( 'trim', explode( ',', $atts['cat'] ) );
-        $args['tax_query'] = [
+        $tax_query = [
             [
                 'taxonomy' => 'tooltipy_category',
                 'field'    => 'slug',
@@ -75,6 +57,59 @@ function custom_tooltipy_glossary( $atts ) {
             ],
         ];
     }
+
+    $current_letter_class = '';
+    if ( empty ( $_GET['letter'] ) ) {
+        $current_letter_class = 'bluet_glossary_current_letter';
+    }
+
+    $all_link = get_permalink();
+
+    echo '<div class="kttg_glossary_div">';
+
+    echo '<div class="kttg_glossary_header"><span class="bluet_glossary_all ' . $current_letter_class . '"><a href=\'' . $all_link . '\'>' . $text_all . '</a></span> - ';
+
+    // Get the letters
+    $chars_count = array();
+    $args = [
+        'post_type'      => 'my_keywords',
+        'post_status'    => 'publish',
+        'posts_per_page' => -1,
+        'orderby'        => 'title',
+        'order'          => 'ASC',
+        'tax_query'      => $tax_query,
+    ];
+    $q = new WP_Query( $args );
+    while ( $q->have_posts() ) {
+        $q->the_post();
+        $my_char = strtoupper( mb_substr( get_the_title(), 0, 1, 'utf-8' ) );
+        if ( empty( $chars_count[$my_char] ) ) {
+            $chars_count[$my_char] = 0;
+        }
+        $chars_count[$my_char]++;
+    }
+    wp_reset_postdata();
+
+    foreach ( $chars_count as $my_char => $nb ) {
+        $current_class = '';
+        $glossary_page_url = get_permalink();
+        $link_to_the_letter_page = add_query_arg( 'letter', $my_char, $glossary_page_url );
+        if ( !empty( $_GET['letter'] ) ) {
+            $current_letter_class = 'bluet_glossary_current_letter';
+        }
+        echo ' <span class="bluet_glossary_letter bluet_glossary_found_letter ' . $current_class . '"><a href=\'' . $link_to_the_letter_page . '\'>' . $my_char . '<span class="bluet_glossary_letter_count">' . $nb . '</span></a></span>';
+    }
+    echo '</div>';
+
+    $args = [
+        'post_type'      => 'my_keywords',
+        'post_status'    => 'publish',
+        'posts_per_page' => -1,
+        'orderby'        => $atts['orderby'],
+        'order'          => $atts['order'],
+        'tax_query'      => $tax_query,
+    ];
+
 
     $q = new WP_Query( $args );
 
