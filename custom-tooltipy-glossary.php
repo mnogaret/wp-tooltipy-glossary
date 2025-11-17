1517de5b092bd8f1afcb93cc9040f30ae9499b40
Mathieu Nogaret <mathieu.nogaret@gmail.com>
2025-11-17 10:44:31 +0100

    Fix

--- custom-tooltipy-glossary.php
+++ custom-tooltipy-glossary.php
@@ -38,13 +38,13 @@ function custom_tooltipy_glossary( $atts ) {
         'custom_tooltipy_glossary'
     );
 
-//    $glossary_options = get_option( 'bluet_glossary_options' );
+    $glossary_options = get_option( 'bluet_glossary_options' );
 
-//    if ( !empty( $glossary_options['kttg_glossary_text']['kttg_glossary_text_all'] ) and $glossary_options['kttg_glossary_text']['kttg_glossary_text_all'] != "" ) {
-//        $text_all = $glossary_options['kttg_glossary_text']['kttg_glossary_text_all'];
-//    } else {
-//        $text_all = __('ALL','tooltipy-lang');
-//    }
+    if ( !empty( $glossary_options['kttg_glossary_text']['kttg_glossary_text_all'] ) and $glossary_options['kttg_glossary_text']['kttg_glossary_text_all'] != "" ) {
+        $text_all = $glossary_options['kttg_glossary_text']['kttg_glossary_text_all'];
+    } else {
+        $text_all = __('ALL','tooltipy-lang');
+    }
 
 //    $current_letter_class = '';
 //    if ( empty ( $_GET['letter'] ) ) {
